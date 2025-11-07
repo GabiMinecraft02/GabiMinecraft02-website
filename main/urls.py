7a563905a3196dd65c05ea64ec50a6e33a71a232
GabrielMinecraft02 <gabrielmersesse09@gmail.com>
2025-11-07 23:25:21 +0100

    Ajout des fichiers HTML et SEO (robots.txt, sitemap.xml)

--- main/urls.py
+++ main/urls.py
@@ -5,8 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('advancements/<str:folder>/', views.advancements, name='advancements'),
-
-    # Google Search Console
     path(
         'googlea128813747473c36.html',
         TemplateView.as_view(template_name='main/googlea128813747473c36.html'),
