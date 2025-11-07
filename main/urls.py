2db35de42507eb6e50adc6bf04521d4ab197ff1f
GabrielMinecraft02 <gabrielmersesse09@gmail.com>
2025-11-07 22:39:47 +0100

    Ajout fichier Google Search Console + nettoyage admin

--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('googlea128813747473c36.html', TemplateView.as_view(template_name='googlea128813747473c36.html')),
     # Page principale
     path('', views.index, name='index'),
 
